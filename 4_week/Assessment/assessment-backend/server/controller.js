74cd2262a36c09ddae733c74cab29cc93513c047
Mahad Ali <mahadaali3@gmail.com>
2023-04-30 21:16:30 -0600

    initial commit

---
+++ 4_week/Assessment/assessment-backend/server/controller.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortune = ["A lifetime friend shall soon be made.", "A lifetime of happiness lies ahead of you.", "Change is happening in your life, so go with the flow!", "Good news will come to you by mail.", "You will travel far and wide, both pleasure and business."];
+      
+        // choose random fortune
+        let randomIndex = Math.floor(Math.random() * fortune.length);
+        let randomFortune = fortune[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
+
+}
